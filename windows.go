94aa0e0a94c0146e28664eac98208ae5786bfcfa
Gefferson Ferraz <me@gefferson.com.br>
2024-08-01 17:19:02 -0300

    fix: change windows time

--- windows.go
+++ windows.go
@@ -19,10 +19,17 @@ func (b *brazilianWords) Sort() string {
 		if i < b.totalWords {
 			result = result + b.separator
 		}
-
+		// REASON: Timer Resolution
 		time.Sleep(20 * time.Millisecond)
 	}
 
 	return result
 
 }
+
+// Timer Resolution - https://pkg.go.dev/time
+/*
+Timer resolution varies depending on the Go runtime, the operating system and the underlying hardware.
+On Unix, the resolution is approximately 1ms. On Windows, the default resolution is approximately 16ms,
+but a higher resolution may be requested using golang.org/x/sys/windows.TimeBeginPeriod.
+*/
